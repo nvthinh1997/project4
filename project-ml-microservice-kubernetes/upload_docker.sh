6ec2d61ebac33ef44192503ce1651d81672aae8b
Thinh <nguyenvanthinh9245@gmail.com>
2022-12-04 20:18:36 +0700

    Update code

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=thinhnv1997/api
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u thinhnv1997
+docker tag api $dockerpath:version1.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:version1.0
